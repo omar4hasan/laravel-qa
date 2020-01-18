8e872fe6f47dad7ea2156517cb7ef1945316931e
omar4hasan <omar4hasan@yahoo.com>
2020-01-18 22:57:53 +0600

    Prepopulate users and question tables using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -9,7 +9,14 @@ class Question extends Model
     protected $fillable = ['title', 'body'];
 
     public function user(){
-        return $this->belongTo(User::class);
+        return $this->belongsTo(User::class);
+
+    }
+    
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = \Str::slug($value);
 
     }
 }
